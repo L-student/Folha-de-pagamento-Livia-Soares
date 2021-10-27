885b37022fdcfd49fd1170364da7684142f6a427
Lívia Soares <61876988+L-student@users.noreply.github.com>
2021-10-27 10:58:12 -0300

    Início em python

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Folha de pagamento")
+print("Por favo, selecione a opção desejada: \n")
+print("1) Adição de um empregado;\n2) Remoção de um empregado;\n3) Lançar um Cartão de Ponto; \n4) Lançar um Resultado Venda;5) Lançar uma taxa de serviço;\n6) Alterar detalhes de um empregado;\n7) Rodar a folha de pagamento para hoje;\n8) Undo/redo;\n9) Agenda de Pagamento;\n10) Criação de Novas Agendas de Pagamento")
+number = int(input())
